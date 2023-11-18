c3a13455f6de57a3244177e69900141cedf5113f
Alexander Malandin <SlendermanGame18@mail.ru>
2023-11-18 10:33:19 +0300

    Create dz7.py

---
+++ dz7.py
@@ -0,0 +1,31 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+
+
+#уровень логов
+logging.basicConfig(level=logging.INFO)
+
+#инициализация
+API_TOKEN='6811574912:AAEVFRisCVX4bqzgXczvp4nmJFa-zNHWus8'
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+#"""TelegramBot"""
+
+@dp.message_handler(commands=['start'])
+async def startbot(message: types.Message):
+    await message.reply("Привет!")
+
+@dp.message_handler(commands=['help'])
+async def helpcom(message: types.Message):
+    await message.reply("Я повторю за тобой и текст и скажу твой id!")
+
+@dp.message_handler()
+async def popugai(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text+', '+msg.from_user.id)
+
+
+
+#Запускаем лог
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
